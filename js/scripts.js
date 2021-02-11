8d6cbe7ff980595f5d0e946b71778239f80e601e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-10 21:09:46 -0800

    Make UI an array, truncate, reverse, lowercase it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-
+function arrayifyTruncateReverse(sentence) {
+  let userArray = sentence.split(" ");
+  let newArray = []
+  userArray.forEach(function(element) {
+    if (element.length >= 3) {
+      newArray.push(element);
+    };
+  });
+  newString = newArray.reverse().join(" ").toLowerCase();
+  return newString;
+}
 
 
 $(document).ready(function(){
